0a2284f91fa43dd824c2f8b9c38506e70bd8924e
Nikita <n.frolov90113@gmail.com>
2025-07-12 15:44:00 +0700

    feature-RAM realisation V4

--- include/mySimpleComputer.h
+++ include/mySimpleComputer.h
@@ -1,17 +1,17 @@
 #ifndef MYSIMPLECOMPUTER_H
 #define MYSIMPLECOMPUTER_H
 
-//Commands
+// Commands
 int sc_commandDecode(int value, int* sign, int* command, int* operand);
 int sc_commandEncode(int sign, int command, int operand, int* value);
 int sc_commandValidate(int command);
 
-//RAM
-void sc_memoryInit ();
-int sc_memorySet (int address, int value);
-int sc_memoryGet (int address, int * value);
-int sc_memorySave (char * filename);
-int sc_memoryLoad (char * filename);
-void printCell (int address);
+// RAM
+void sc_memoryInit();
+int sc_memorySet(int address, int value);
+int sc_memoryGet(int address, int* value);
+int sc_memorySave(char* filename);
+int sc_memoryLoad(char* filename);
+void printCell(int address);
 
 #endif // MYSIMPLECOMPUTER_H
