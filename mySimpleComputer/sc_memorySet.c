d74521f954c7328afc018d8801e573a5e03f486c
Nikita <n.frolov90113@gmail.com>
2025-07-12 15:40:19 +0700

    feature-RAM realisation V3

--- mySimpleComputer/sc_memorySet.c
+++ mySimpleComputer/sc_memorySet.c
@@ -2,7 +2,7 @@
 #include "./sc_constants.h"
 #include "sc_variables.h"
 
-int sc_memorySet (int address, int value)
+int sc_memorySet(int address, int value)
 {
     if (address < RAM_MIN_ADDRESS || address > RAM_MAX_ADDRESS || value > RAM_MAX_VALUE || value < RAM_MIN_VALUE)
     {
--- mySimpleComputer/sc_variables.c
+++ mySimpleComputer/sc_variables.c
@@ -1,2 +1,4 @@
 #include "sc_variables.h"
+#include "sc_constants.h"
+
 int RAM[RAM_SIZE];
