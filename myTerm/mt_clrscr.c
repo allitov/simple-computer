d7b961a502286ae1e2993c0cab124e269337e716
Alexander Litovkin <litovkinalexan@gmail.com>
2025-07-26 17:08:02 +0700

    add console constants

--- myTerm/mt_clrscr.c
+++ myTerm/mt_clrscr.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "../include/constants.h"
 #include "../include/myTerm.h"
+#include "mt_constants.h"
 
 int mt_clrscr(void)
 {
-    printf("\033[2J\033[H");
+    printf(CLEAR_SCREEN);
     fflush(stdout);
 
     return OK;
---
+++ myTerm/mt_constants.h
@@ -0,0 +1,7 @@
+#ifndef MT_CONSTANTS_H
+#define MT_CONSTANTS_H
+
+#define CLEAR_SCREEN "\033[2J\033[H"
+#define GOTO_YX "\033[%d;%dH"
+
+#endif //MT_CONSTANTS_H
