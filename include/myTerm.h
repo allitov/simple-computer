67aa53447c60e76b9c0e8fc51de01e6535c87326
FedosOffline <ohdude2003fed@gmail.com>
2025-07-27 15:02:47 +0700

    fix spaces

--- include/myTerm.h
+++ include/myTerm.h
@@ -5,12 +5,10 @@
 
 int mt_clrscr(void);
 int mt_gotoXY(int x, int y);
-int mt_getscreensize(int* rows, int* cols);
+int mt_getscreensize(int *rows, int *cols);
 int mt_setfgcolor(enum colors color);
 int mt_setbgcolor(enum colors color);
 int mt_setdefaultcolor(void);
-
 int mt_setcursorvisible(int value);
-
 int mt_delline(void);
 #endif // MYTERM_H
